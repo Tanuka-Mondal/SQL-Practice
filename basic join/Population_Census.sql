fd020d57f64b15d3480c5aecc29ade06181471d5
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-08-17 01:09:06 +0530

    Create Population_Census.sql

---
+++ basic join/Population_Census.sql
@@ -0,0 +1,9 @@
+/*
+Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+SELECT SUM(CITY.POPULATION) 
+FROM CITY, COUNTRY
+WHERE CITY.COUNTRYCODE = COUNTRY.CODE 
+    AND COUNTRY.CONTINENT = 'Asia';
