39628d724de8f53b88c09c37bce62bb38b0e19ff
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-08-16 20:20:22 +0530

    Create Weather Observation Station 9.sql

---
+++ basic select/Weather Observation Station 9.sql
@@ -0,0 +1,4 @@
+--Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT(CITY)
+FROM STATION
+WHERE LEFT(CITY,1) NOT IN ('a','e','i','o','u');
