0c1a7c8380c6e57cb2f0b8e0e46b5018e2899923
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2022-09-23 02:32:26 +0530

    Update Employee_Salaries.sql

--- basic select/Employee_Salaries.sql
+++ basic select/Employee_Salaries.sql
@@ -1,6 +1,6 @@
 /*
-Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than  per month who have been employees 
-for less than  months. Sort your result by ascending employee_id.
+Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than 2000  per month who have been employees 
+for less than 10 months. Sort your result by ascending employee_id.
 */
 SELECT NAME
 FROM EMPLOYEE
